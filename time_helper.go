06fd155852b4731ab29f4c217a4a120caf68ffd6
kobeld <kobeld@163.com>
2015-01-17 22:05:56 +0800

    Add a parse time function

---
+++ time_helper.go
@@ -0,0 +1,31 @@
+package goutils
+
+import (
+	"time"
+)
+
+// Compare to the offical time.Parse, this ignore the error message and
+// return a blank time.Time{} object when value has something wrong
+func ParseTime(layout, value string) time.Time {
+	if value == "" {
+		return time.Time{}
+	}
+
+	r, err := time.Parse(layout, value)
+	if err != nil {
+		PrintStackAndError(err)
+		return time.Time{}
+	}
+
+	return r
+}
+
+// Compare to the offical time.Format, it will return blank string when
+// the time is zero, rather than return "0001-01-01 00:00"
+func FormatTime(theTime time.Time, layout string) (r string) {
+	if theTime.IsZero() {
+		return ""
+	}
+
+	return theTime.Format(layout)
+}
